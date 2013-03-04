358521412d6a90577673778fb3cf95e03e3c2127
Vojtech Hyza <vhyza@vhyza.eu>
2013-03-04 20:31:48 +0100

    Better history

--- oh-my-zsh/custom/custom_settings.zsh
+++ oh-my-zsh/custom/custom_settings.zsh
@@ -3,6 +3,12 @@ autoload -U select-word-style
 select-word-style bash
 bindkey '^X^E' edit-command-line
 
+autoload history-search-end
+zle -N history-beginning-search-backward-end history-search-end
+zle -N history-beginning-search-forward-end history-search-end
+bindkey "\e[A" history-beginning-search-backward-end  # cursor up
+bindkey "\e[B" history-beginning-search-forward-end   # cursor down
+
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 export EDITOR="s -w -n"
