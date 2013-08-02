f129b7c4c8d23570e253b757cd842ca06cf6f355
Vojtech Hyza <vhyza@vhyza.eu>
2013-08-02 13:15:44 +0200

    Removed swap switching off

--- mac/customize.sh
+++ mac/customize.sh
@@ -339,8 +339,6 @@ function set_application_preferences () {
 
   /usr/bin/defaults write com.apple.DiskUtility 'DUDebugMenuEnabled' -bool true
 
-  ## Disable swap
-  /bin/launchctl unload -w /System/Library/LaunchDaemons/com.apple.dynamic_pager.plist
 
 
   ## Disable hibernation
